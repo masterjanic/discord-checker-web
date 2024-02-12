c94757d02f3e2d57ce03160c88aa1fed0984fac0
masterjanic <68118279+masterjanic@users.noreply.github.com>
2024-02-12 09:04:04 +0100

    feat: add copy function to tokens

--- src/components/customer/accounts/account-tokens/account-tokens-card.tsx
+++ src/components/customer/accounts/account-tokens/account-tokens-card.tsx
@@ -2,11 +2,22 @@
 
 import { type DiscordToken } from "@prisma/client";
 import { useState } from "react";
-import { PiEyeBold, PiEyeSlashBold, PiSignInBold } from "react-icons/pi";
+import {
+  PiCopyDuotone,
+  PiEyeBold,
+  PiEyeSlashBold,
+  PiSignInBold,
+} from "react-icons/pi";
 
 import TitledCard from "~/components/common/titled-card";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
 import { getRelativeTime } from "~/lib/time";
 import { api } from "~/trpc/react";
 
@@ -16,19 +27,36 @@ function TokenEntry({
   token: Pick<DiscordToken, "id" | "value" | "lastCheckedAt">;
 }) {
   const [isVisible, setIsVisible] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
 
   return (
-    <div className="flex flex-col space-y-2">
+    <div className="flex flex-col space-y-1.5">
       <div className="flex items-center space-x-2">
-        {
-          // TODO: Copy on click with notification
-        }
-        <Input
-          name={`token-${token.id}`}
-          type={isVisible ? "text" : "password"}
-          value={token.value}
-          readOnly
-        />
+        <TooltipProvider>
+          <Tooltip open={isCopied}>
+            <TooltipTrigger asChild>
+              <Input
+                name={`token-${token.id}`}
+                type={isVisible ? "text" : "password"}
+                value={token.value}
+                className="cursor-pointer select-none"
+                onClick={async () => {
+                  setIsCopied(true);
+                  await navigator.clipboard.writeText(token.value);
+                  setTimeout(() => {
+                    setIsCopied(false);
+                  }, 2000);
+                }}
+                readOnly
+              />
+            </TooltipTrigger>
+            <TooltipContent className="flex items-center space-x-1.5">
+              <PiCopyDuotone className="h-4 w-4" />
+              <span>Copied to clipboard</span>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
         <div className="flex items-center space-x-1">
           <Button
             variant="outline"
@@ -46,10 +74,6 @@ function TokenEntry({
             <span className="sr-only">Fast Login with Chrome extension</span>
             <PiSignInBold />
           </Button>
-
-          {
-            // TODO: Delete Button?
-          }
         </div>
       </div>
       <small className="font-light ml-1.5">
@@ -64,9 +88,14 @@ export default function AccountTokensCard({ userId }: { userId: string }) {
 
   return (
     <TitledCard title="Account Tokens">
-      {tokens.map((token) => (
-        <TokenEntry key={`account-${userId}-token-${token.id}`} token={token} />
-      ))}
+      <div className="space-y-2">
+        {tokens.map((token) => (
+          <TokenEntry
+            key={`account-${userId}-token-${token.id}`}
+            token={token}
+          />
+        ))}
+      </div>
     </TitledCard>
   );
 }
