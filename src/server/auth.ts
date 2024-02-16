ee07fc0d34bd6b5ff63d0fc5d5324502055d0576
masterjanic <68118279+masterjanic@users.noreply.github.com>
2024-02-16 16:23:01 +0100

    feat: customize email message

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,8 +8,11 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import EmailProvider from "next-auth/providers/email";
+import EmailProvider, {
+  type SendVerificationRequestParams,
+} from "next-auth/providers/email";
 import GitHubProvider from "next-auth/providers/github";
+import nodemailer from "nodemailer";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -27,6 +30,78 @@ declare module "next-auth" {
     role: Role;
     subscribedTill?: Date;
   }
+}
+
+async function sendVerificationRequest({
+  identifier: email,
+  url,
+  provider: { server, from },
+}: SendVerificationRequestParams) {
+  const { host } = new URL(url);
+  const transport = nodemailer.createTransport(server);
+  await transport.sendMail({
+    to: email,
+    from,
+    subject: `Verify your Login | DTC-Web`,
+    text: text({ url, host }),
+    html: html({ url, host, email }),
+  });
+}
+
+// Email HTML body
+function html({ url, host, email }: Record<"url" | "host" | "email", string>) {
+  // Insert invisible space into domains and email address to prevent both the
+  // email address and the domain from being turned into a hyperlink by email
+  // clients like Outlook and Apple mail, as this is confusing because it seems
+  // like they are supposed to click on their email address to sign in.
+  const escapedEmail = `${email.replace(/\./g, "&#8203;.")}`;
+  const escapedHost = `${host.replace(/\./g, "&#8203;.")}`;
+
+  // Some simple styling options
+  const backgroundColor = "#f9f9f9";
+  const textColor = "#444444";
+  const mainBackgroundColor = "#ffffff";
+  const buttonBackgroundColor = "#346df1";
+  const buttonBorderColor = "#346df1";
+  const buttonTextColor = "#ffffff";
+
+  return `
+<body style="background: ${backgroundColor};">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        <strong>${escapedHost}</strong>
+      </td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        Sign in as <strong>${escapedEmail}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>
+`;
+}
+
+// Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
+function text({ url, host }: Record<"url" | "host", string>) {
+  return `Sign in to ${host}\n${url}\n\n`;
 }
 
 /**
@@ -157,6 +232,7 @@ export const authOptions: NextAuthOptions = {
       server: env.EMAIL_SERVER,
       from: env.EMAIL_FROM,
       maxAge: 5 * 60, // valid for 5 minutes
+      sendVerificationRequest,
     }),
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
