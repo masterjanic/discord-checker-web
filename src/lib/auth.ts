79b694ebbe094bad25b2deb6d5a5c0110aa2b41c
Janic <68118279+masterjanic@users.noreply.github.com>
2024-03-14 14:41:37 +0100

    fix(auth): cleanup this type hell

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,44 +1,52 @@
-import { Role, type User } from "@prisma/client";
+import { eq, type SQL } from "drizzle-orm";
+import { type PgColumn } from "drizzle-orm/pg-core";
 import { type Session } from "next-auth";
 
+import { UserRole } from "~/server/db/schema";
+
 /**
- * Returns the owner id of a user based on their role.
- * @param role
- * @param id
+ * Returns a query condition based on the user's role.
+ * This is used to distinguish between regular users and administrators for protected queries and
+ * mutations.
+ *
+ * @param user The user to check the role of.
+ * @param column The column to compare the user's ID to.
  */
-export const getOwnerId = ({ role, id }: Pick<User, "id" | "role">) => {
-  const isAdmin = role === Role.ADMIN;
-  return isAdmin ? undefined : id;
+export const conditionalQuery = (
+  user: Pick<Session["user"], "id" | "role">,
+  column: PgColumn,
+): SQL | undefined => {
+  const isAdmin = isAdministrator(user);
+  return isAdmin ? undefined : eq(column, user.id);
 };
 
 /**
- * Returns whether a user is subscribed or not.
- * @param user
+ * A shorthand function to check if a user is subscribed.
+ * If the user is an administrator, they are considered subscribed.
+ *
+ * @param user The user to check the subscription status of.
  */
 export const isUserSubscribed = (
-  user:
-    | {
-        role?: Session["user"]["role"] | null | undefined;
-        subscribedTill?: Session["user"]["subscribedTill"] | null | undefined;
-      }
-    | undefined,
+  user?: Pick<Session["user"], "subscribedTill" | "role">,
 ): boolean => {
-  return (
-    isAdministrator(user) ||
-    (!!user?.subscribedTill && new Date() < new Date(user.subscribedTill))
-  );
+  if (!user) {
+    return false;
+  }
+
+  if (isAdministrator(user)) {
+    return true;
+  }
+
+  return !!user.subscribedTill && new Date() < new Date(user.subscribedTill);
 };
 
 /**
- * Returns whether an authed user is an administrator or not.
- * @param user
+ * A shorthand function to check if a user is an administrator.
+ *
+ * @param user The user to check the role of.
  */
 export const isAdministrator = (
-  user:
-    | {
-        role?: Session["user"]["role"] | null | undefined;
-      }
-    | undefined,
+  user?: Pick<Session["user"], "role">,
 ): boolean => {
-  return user?.role === Role.ADMIN;
+  return user?.role === UserRole.ADMIN;
 };
